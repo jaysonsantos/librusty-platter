2d63779c43529a7422d8c6c44abbff5796d352c2
Jayson Reis (jaysonsantos) <santosdosreis@gmail.com>
2017-04-04 22:04:06 +0200

    wip

--- src/fs/encrypted.rs
+++ src/fs/encrypted.rs
@@ -1,13 +1,15 @@
 use base64::encode;
 
+use fs::Filesystem;
+use result::RustyPlatterResult;
+
 use ring::aead::{SealingKey, OpeningKey, CHACHA20_POLY1305, seal_in_place};
 use ring::pbkdf2;
-use ring::rand::SystemRandom;
+use ring::rand::{SystemRandom, SecureRandom};
 
 use serde_json;
 
-use fs::Filesystem;
-use result::RustyPlatterResult;
+const MINIMUM_ENCRYPTION_SIZE: usize = 16;
 
 struct Keys {
     opening: OpeningKey,
@@ -25,6 +27,7 @@ struct Config {
 }
 
 impl Config {
+    #![allow(dead_code)]
     fn new(password: &str, iterations: u32, fs: &Filesystem) -> RustyPlatterResult<Self> {
         let mut salt = [0u8; 16];
         let rand = SystemRandom::new();
@@ -48,7 +51,7 @@ impl Config {
             keys: Some(keys),
         };
 
-        config.save(fs);
+        config.save(fs)?;
 
         Ok(config)
     }
@@ -63,6 +66,7 @@ impl Config {
 struct EncryptedFs<'a> {
     fs: &'a Filesystem,
     config: Config,
+    random: Box<SecureRandom>,
 }
 
 impl<'a> EncryptedFs<'a> {
@@ -70,21 +74,34 @@ impl<'a> EncryptedFs<'a> {
         EncryptedFs {
             fs: fs,
             config: config,
+            random: Box::new(SystemRandom::new()),
+        }
+    }
+
+    fn with_custom_random(fs: &'a Filesystem, config: Config, random: Box<SecureRandom>) -> Self {
+        EncryptedFs {
+            fs: fs,
+            config: config,
+            random: random,
         }
     }
 
     fn encrypt_name(&self, name: &str) -> RustyPlatterResult<String> {
-        assert!(name.len() >= 16);  // TODO: Add some padding
         let keys = self.config.keys.as_ref().unwrap();
         let additional_data = [];
         let mut nonce = vec![0; 12];
         let mut output: Vec<u8> = vec![];
         let mut to_encrypt: Vec<u8> = vec![];
         to_encrypt.extend_from_slice(name.as_bytes());
+        if let Some(remaining) = MINIMUM_ENCRYPTION_SIZE.checked_sub(to_encrypt.len()) {
+            for _ in 0..remaining {
+                to_encrypt.push(0);
+            }
+        }
 
         // Fill nonce with random data
-        let rand = SystemRandom::new();
-        rand.fill(&mut nonce);
+        self.random.fill(&mut nonce)?;
+        println!("{:?}", nonce);
         output.extend_from_slice(&nonce);
 
         seal_in_place(&keys.sealing,
@@ -107,17 +124,33 @@ mod tests {
     extern crate ring;
     extern crate tempdir;
 
+    use fs::local::LocalFileSystem;
+
     use ring::aead::*;
+    use ring::error::Unspecified;
     use ring::pbkdf2::*;
     use ring::rand::SystemRandom;
 
     use self::tempdir::TempDir;
+    use std::io::Write;
 
     use super::*;
-    use fs::local::LocalFileSystem;
 
     const PASSWORD: &'static str = "password";
     const ITERATIONS: u32 = 100;
+
+    struct DumbRandom {}
+    impl DumbRandom {
+        fn dumb_data(&self) -> Vec<u8> {
+            (0..100).collect()
+        }
+    }
+    impl SecureRandom for DumbRandom {
+        fn fill(&self, mut buf: &mut [u8]) -> Result<(), Unspecified> {
+            buf.write(&self.dumb_data()).unwrap();
+            Ok(())
+        }
+    }
 
     #[test]
     fn test_encrypt_name() {
@@ -126,8 +159,8 @@ mod tests {
         let fs = LocalFileSystem::new(path.to_str().unwrap());
 
         let config = Config::new(PASSWORD, ITERATIONS, &fs).unwrap();
-        let encrypted = EncryptedFs::new(&fs, config);
-        let encrypted_name = encrypted.encrypt_name("abc").unwrap();
+        let encrypted = EncryptedFs::with_custom_random(&fs, config, Box::new(DumbRandom {}));
+        let encrypted_name = encrypted.encrypt_name("abcd").unwrap();
         assert_eq!(encrypted_name, "abc");
     }
 
